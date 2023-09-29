e7e8cc978373a2c5404c381139a4efff4cb5728d
Mayank Uchariya <mayankuchariya0@gmail.com>
2023-09-29 14:50:23 +0530

    Final_commit

--- pages/api/getproducts.js
+++ pages/api/getproducts.js
@@ -1,5 +1,5 @@
 import connectDb from "../../middlewear/mongoose"
-import Product from "../models/Product"
+import Product from "../../models/Product"
 
 const handler = async(req,res)=>{
     let products = await Product.find()
