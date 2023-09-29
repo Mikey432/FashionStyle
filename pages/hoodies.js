4afc221b8fed6a0b9d241517f4f0c82e99a6ceb5
Mayank Uchariya <mayankuchariya0@gmail.com>
2023-09-29 14:29:03 +0530

    final commit

--- pages/hoodies.js
+++ pages/hoodies.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import mongoose from "mongoose";
-import Product from '../models/Product';
+import Product from '../../models/Product';
 
 const Hoodies = ({products}) => {
   return (
